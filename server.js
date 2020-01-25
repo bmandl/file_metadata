ac23052127fb81307b65699662d693248e2464dd
Blaž Mandl <blaz.mandl@gmail.com>
2020-01-25 21:02:13 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
-'use strict';
 
 var express = require('express');
 var cors = require('cors');
-
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+var app = express();
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 // require and use "multer"...
 
-var app = express();
-
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(express.static(process.cwd() + '/public')); //heroku
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.sendFile(process.cwd() + '/views/index.html'); //heroku
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
-});
+// Not found middleware
+app.use((req, res, next) => {
+  return next({ status: 404, message: 'not found' })
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT || 5000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
